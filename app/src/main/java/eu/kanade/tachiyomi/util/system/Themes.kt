d2c3fccb3696e70466f85505d0339f5de3636f85
Jays2Kings <jays@outlook.com>
2026-08-03 15:55:04 -0700

    Add Tokyo Night theme
    Co-Authored-By: Mohannad <134175138+xmohnad@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/util/system/Themes.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/Themes.kt
@@ -48,6 +48,12 @@ enum class Themes(
         R.string.lavender,
         R.string.violet,
     ),
+    TOKYO_NIGHT(
+        R.style.Theme_Tachiyomi_TokyoNight,
+        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM,
+        R.string.tokyo_light,
+        R.string.tokyo_night,
+    ),
     TAKO(
         R.style.Theme_Tachiyomi_Tako,
         AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM,
